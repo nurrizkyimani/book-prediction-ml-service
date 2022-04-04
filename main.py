0fc94943734ac77386b29c54050522ceff78fdbb
nurrizkyimn <nurrizkyimanidev@gmail.com>
2022-04-04 07:16:49 +0700

    adding redis stream in main py

--- main.py
+++ main.py
@@ -5,54 +5,58 @@ from random import seed
 import random
 import numpy as np
 import pickle
-
-from pika_client import PikaClient
-from schema import MessageSchema
-
+from kafka import KafkaProducer
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
 from fastapi import Request
+from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:8080'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
-model_pick = pickle.load(open('model.pkl', 'rb'))
 
-
-@app.post('/send-message')
-async def send_message(payload: MessageSchema, request: Request):
-    request.app.pika_client.send_message(
-        {"message": payload.message}
-    )
-    return {"status": "ok"}
+redis = get_redis_connection(
+    host="redis-16079.c299.asia-northeast1-1.gce.cloud.redislabs.com",
+    port=16079,
+    password="9E4RzDu2edr8QdyFOH8aSWaBrVl5hCSi",
+    decode_responses=True
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Empty root"}
+    return {"message": "This is empty root"}
+
+example_dict = {'name': 'ricky', 'age': 12}
 
 
-@app.post("/salary_predict")
-async def predict():
-    data = request.get_json(force=true)
-    prediction = model_pick.prediction([[np.array(data['exp'])]])
-    output = prediction[0]
-    return jsonify(output)
+@app.get("/random_streamline")
+async def streaming_prediction_to_database():
+    redis.xadd("test_union", example_dict, "*")
+    return {"message": "send a data to redis pipeline :)"}
 
 
 @app.get("/book_prediction")
-def get_book_prediction():
+async def get_book_prediction():
 
     url_rating = './ratings.csv'
     url_books = './books.csv'
 
     rating = pd.read_csv(url_rating)
-
     books = pd.read_csv(url_books)
 
     book_data = np.array(list(set(rating.book_id)))
-    user = np.array([100 for i in range(len(book_data))])
+    user = np.array([101 for i in range(len(book_data))])
     model = keras.models.load_model('models')
     # model.summary()
 
@@ -71,3 +75,7 @@ def get_book_prediction():
         "status": 200,
         "docs": lit
     }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8080)
